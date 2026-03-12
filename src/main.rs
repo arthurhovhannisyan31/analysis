63ebed05c89604b218de1580700bbde2892e4905
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-03-12 22:03:20 +0400

    replace parse functions with single generic parser

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@
 //  -- по ошибкам
 //  -- по изменению счёта (купить/продать)
 
+use analysis::parse::Announcements;
+
 // Модель данных:
 // - Пользователь (userid, имя)
 // - Вещи
@@ -52,23 +54,24 @@ fn main() {
   println!("Placeholder для экспериментов с cli");
 
   let parsing_demo = r#"[UserBackets{"user_id":"Bob","backets":[Backet{"asset_id":"milk","count":3,},],},]"#.to_string();
+  // Announcements
   let announcements =
-    analysis::parse::just_parse_anouncements(parsing_demo).unwrap();
+    analysis::parse::parse_asset::<Announcements>(parsing_demo).unwrap();
   println!("demo-parsed: {:?}", announcements);
 
-  let args = std::env::args().collect::<Vec<_>>();
-  let filename = args[1].clone();
-  println!(
-    "Trying opening file '{}' from directory '{}'",
-    filename,
-    std::env::current_dir().unwrap().to_string_lossy()
-  );
-  let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> =
-    std::rc::Rc::new(std::cell::RefCell::new(Box::new(
-      std::fs::File::open(filename).unwrap(),
-    )));
-
-  let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
-  println!("got logs:");
-  logs.iter().for_each(|parsed| println!("  {:?}", parsed));
+  // let args = std::env::args().collect::<Vec<_>>();
+  // let filename = args[1].clone();
+  // println!(
+  //   "Trying opening file '{}' from directory '{}'",
+  //   filename,
+  //   std::env::current_dir().unwrap().to_string_lossy()
+  // );
+  // let file: std::rc::Rc<std::cell::RefCell<Box<dyn analysis::MyReader>>> =
+  //   std::rc::Rc::new(std::cell::RefCell::new(Box::new(
+  //     std::fs::File::open(filename).unwrap(),
+  //   )));
+  //
+  // let logs = analysis::read_log(file.clone(), analysis::READ_MODE_ALL, vec![]);
+  // println!("got logs:");
+  // logs.iter().for_each(|parsed| println!("  {:?}", parsed));
 }
