59a262257c1556196a16d6144729b7ae236e9e90
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-03-12 10:34:09 +0400

    add code formatting

--- src/lib.rs
+++ src/lib.rs
@@ -13,69 +13,80 @@ pub const READ_MODE_EXCHANGES: u8 = 2;
 #[derive(Debug)]
 struct RefMutWrapper<'a, T>(std::cell::RefMut<'a, T>);
 impl<'a, T> std::io::Read for RefMutWrapper<'a, T>
-where T: std::io::Read
+where
+  T: std::io::Read,
 {
-    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        self.0.read(buf)
-    }
+  fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+    self.0.read(buf)
+  }
 }
 
 /// Для `Box<dyn много трейтов, помимо auto-трейтов>`, (`rustc E0225`)
 /// `only auto traits can be used as additional traits in a trait object`
 /// `consider creating a new trait with all of these as supertraits and using that trait here instead`
-pub trait MyReader: std::io::Read + std::fmt::Debug + 'static
-{}
-impl<T: std::io::Read + std::fmt::Debug + 'static> MyReader for T
-{}
+pub trait MyReader: std::io::Read + std::fmt::Debug + 'static {}
+impl<T: std::io::Read + std::fmt::Debug + 'static> MyReader for T {}
 // подсказка: вместо trait-объекта можно дженерик
 /// Итератор, на выходе которого - строки распарсенной структуры данных
 #[derive(Debug)]
 struct LogIterator {
-    lines: std::iter::Filter<std::io::Lines<std::io::BufReader<RefMutWrapper<'static, Box<dyn MyReader>>>>,fn(&Result<String,std::io::Error>)->bool>,
-    reader_rc: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>,
+  lines: std::iter::Filter<
+    std::io::Lines<
+      std::io::BufReader<RefMutWrapper<'static, Box<dyn MyReader>>>,
+    >,
+    fn(&Result<String, std::io::Error>) -> bool,
+  >,
+  reader_rc: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>,
 }
 impl LogIterator {
-    fn new(r: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>) -> Self {
-        use std::io::BufRead;
-        // подсказка: unsafe избыточен, да и весь rc - тоже
-        // примечание автора прототипа:
-        // > Мотивация: хочу позаимствовать RefCell,
-        // > но боюсь, что Rc протухнет - поэтому буду хранить и Rc и RefMut.
-        // > Я знаю, что деструкторы полей структуры вызываются в
-        // > порядке объявления в структуре - то есть сначала будет удалён
-        // > мой RefMutWrapper, а уже потом и весь исходный reader_rc
-        let the_borrow = r.borrow_mut();
-        let the_borrow = unsafe{std::mem::transmute::<_,_>(the_borrow)};
-        Self{
-            lines: std::io::BufReader::with_capacity(4096, RefMutWrapper(the_borrow))
-                       .lines()
-                       .filter(
-                           |line_res|
-                           !line_res.as_ref().ok()
-                               .map(|line| line.trim().is_empty())
-                               .unwrap_or(false)
-                        ),
-            reader_rc: r,
-        }
+  fn new(r: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>) -> Self {
+    use std::io::BufRead;
+    // подсказка: unsafe избыточен, да и весь rc - тоже
+    // примечание автора прототипа:
+    // > Мотивация: хочу позаимствовать RefCell,
+    // > но боюсь, что Rc протухнет - поэтому буду хранить и Rc и RefMut.
+    // > Я знаю, что деструкторы полей структуры вызываются в
+    // > порядке объявления в структуре - то есть сначала будет удалён
+    // > мой RefMutWrapper, а уже потом и весь исходный reader_rc
+    let the_borrow = r.borrow_mut();
+    let the_borrow = unsafe { std::mem::transmute::<_, _>(the_borrow) };
+    Self {
+      lines: std::io::BufReader::with_capacity(4096, RefMutWrapper(the_borrow))
+        .lines()
+        .filter(|line_res| {
+          !line_res
+            .as_ref()
+            .ok()
+            .map(|line| line.trim().is_empty())
+            .unwrap_or(false)
+        }),
+      reader_rc: r,
     }
+  }
 }
 impl Iterator for LogIterator {
-    type Item = parse::LogLine;
-    fn next(&mut self) -> Option<Self::Item> {
-        let line = self.lines.next()?.ok()?;
-        let (remaining, result) = LOG_LINE_PARSER.parse(line.trim().to_string()).ok()?;
-        remaining.trim().is_empty().then_some(result)
-    }
+  type Item = parse::LogLine;
+  fn next(&mut self) -> Option<Self::Item> {
+    let line = self.lines.next()?.ok()?;
+    let (remaining, result) =
+      LOG_LINE_PARSER.parse(line.trim().to_string()).ok()?;
+    remaining.trim().is_empty().then_some(result)
+  }
 }
 
 // подсказка: RefCell вообще не нужен
 /// Принимает поток байт, отдаёт отфильтрованные и распарсенные логи
-pub fn read_log(input: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>, mode: u8, request_ids: Vec<u32>) -> Vec<LogLine> {
-    let logs = LogIterator::new(input);
-    let mut collected = Vec::new();
-    // подсказка: можно обойтись итераторами
-    for log in logs {
-        if request_ids.is_empty() || {
+pub fn read_log(
+  input: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>,
+  mode: u8,
+  request_ids: Vec<u32>,
+) -> Vec<LogLine> {
+  let logs = LogIterator::new(input);
+  let mut collected = Vec::new();
+  // подсказка: можно обойтись итераторами
+  for log in logs {
+    if request_ids.is_empty()
+      || {
             let mut request_id_found = false;
             for request_id in &request_ids {
                 if *request_id == log.request_id {
@@ -114,21 +125,21 @@ pub fn read_log(input: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>>, mode:
                 // подсказка: паниковать в библиотечном коде - нехорошо
                 panic!("unknown mode {}", mode)
             }
-        {
-            collected.push(log);
-        }
+    {
+      collected.push(log);
     }
-    collected
+  }
+  collected
 }
-
 
 #[cfg(test)]
 mod test {
-    use super::*;
+  use super::*;
 
-    const SOURCE1: &'static str = r#"System::Error NetworkError "url unknown" requestid=1"#;
+  const SOURCE1: &'static str =
+    r#"System::Error NetworkError "url unknown" requestid=1"#;
 
-    const SOURCE: &'static str = r#"
+  const SOURCE: &'static str = r#"
 System::Error NetworkError "network interface is down" requestid=1
 App::Error SystemError "network" requestid=1
 System::Trace SendRequest "CreateUser{\"user_id\": 10, \"authrized_capital\": 1000,}" requestid=2
@@ -192,17 +203,20 @@ App::Trace GetResponse "Ok" requestid=10
 App::Journal BuyAsset UserBacket{"user_id":"Alice","backet":Backet{"asset_id":"milk","count":5,},} requestid=10
         "#;
 
-
-    #[test]
-    fn test_all() {
-        let refcell1: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>> = std::rc::Rc::new(std::cell::RefCell::new(Box::new(SOURCE1.as_bytes())));
-        assert_eq!(read_log(refcell1.clone(), READ_MODE_ALL, vec![]).len(), 1);
-        let refcell: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>> = std::rc::Rc::new(std::cell::RefCell::new(Box::new(SOURCE.as_bytes())));
-        let all_parsed = read_log(refcell.clone(), READ_MODE_ALL, vec![]);
-        println!("all parsed:");
-        all_parsed.iter().for_each(|parsed| println!("  {:?}", parsed));
-        // 2 для начала и конца строки (чтобы первая и последняя кавычки на отдельных строках были)
-        // второе число - число пустых строк, которые оставлены для удобства чтения
-        assert_eq!(all_parsed.len(), SOURCE.lines().count()-2-7);
-    }
+  #[test]
+  fn test_all() {
+    let refcell1: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>> =
+      std::rc::Rc::new(std::cell::RefCell::new(Box::new(SOURCE1.as_bytes())));
+    assert_eq!(read_log(refcell1.clone(), READ_MODE_ALL, vec![]).len(), 1);
+    let refcell: std::rc::Rc<std::cell::RefCell<Box<dyn MyReader>>> =
+      std::rc::Rc::new(std::cell::RefCell::new(Box::new(SOURCE.as_bytes())));
+    let all_parsed = read_log(refcell.clone(), READ_MODE_ALL, vec![]);
+    println!("all parsed:");
+    all_parsed
+      .iter()
+      .for_each(|parsed| println!("  {:?}", parsed));
+    // 2 для начала и конца строки (чтобы первая и последняя кавычки на отдельных строках были)
+    // второе число - число пустых строк, которые оставлены для удобства чтения
+    assert_eq!(all_parsed.len(), SOURCE.lines().count() - 2 - 7);
+  }
 }
